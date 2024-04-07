22adecd32a32902c74a2a924fbdf63a363701c8c
vaneath <vaneathzz@gmail.com>
2024-04-07 19:31:24 +0700

    close #17 revert open admin book controller

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,29 +1,37 @@
 <nav class="border-b-4 border-rose-200 m-auto">
 
     <div class="bg-black h-24 text-white">
-     
-        <div class="grid grid-cols-2 gap-4">
-            <a href="/" class="px-24 py-2">
-                <img src="https://firebasestorage.googleapis.com/v0/b/abhc-63252.appspot.com/o/filemanager%2Fdae6n1651481918290logo%5B1%5D.png?alt=media&token=9c752acb-db5e-4242-9e5b-ba8d98271fb9" class="h-20" alt="Flowbite Logo" />
+
+        <div class="flex justify-evenly">
+            <a href="/">
+                <img src="https://firebasestorage.googleapis.com/v0/b/abhc-63252.appspot.com/o/filemanager%2Fdae6n1651481918290logo%5B1%5D.png?alt=media&token=9c752acb-db5e-4242-9e5b-ba8d98271fb9"
+                    class="h-20" alt="Flowbite Logo" />
             </a>
-            {{-- <div class=" items-center sm:hidden block md:show">
-                <input type="text" placeholder="Search" class="rounded-full flex ">
-              </div> --}}
-            <div class="">
-                <div class="hidden lg:block grid grid-cols-2 gap-4 px-24 py-8">
-                    <a href="tel:+85578667721" class="text-sm  text-amber-100 hover:text-amber-300">(+855)78 66 77 21</a>
-                    <a href="/login" class=" text-sm text-amber-100 hover:text-amber-300 pl-24">Login</a>
-                </div>
-                <div class="lg:hidden grid grid-cols-2 gap-4 px-24 py-8">
-                    <a href="tel:+85578667721"><i class="fa-solid fa-phone "></i></a>
-                    <a href="/login"><i class="fa-regular fa-user "></i></a>
-                </div>
-              
+            <div>
+                @if (Auth::check())
+                    <div class="flex justify-between space-x-16 ">
+                        <a href="/dashboard" class="text-sm  text-white">Dashboard</a>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                              this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </div>
+                @else
+                    <div class="flex justify-between space-x-16 ">
+                        <a href="/register" class="text-sm  text-white ">Register</a>
+                        <a href="/login" class=" text-sm text-white">Login</a>
+                    </div>
+                @endif
             </div>
-           
+
         </div>
     </div>
-    
+
     <div class="bg-white flex justify-center">
         <div class="max-w-screen-xl px-4 py-6 mx-auto">
             <div class="flex items-center ">
@@ -43,9 +51,9 @@
                 </ul>
             </div>
         </div>
-        
+
     </div>
-    
-    
+
+
 
 </nav>
